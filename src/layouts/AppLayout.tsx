4970b97bf5834253de5678dd1ee04eaecc343ffe
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-22 01:45:05 +0300

    feat:app is wrapped by blank layout

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import React, { FunctionComponent, ReactElement } from 'react'
+import Navbar from '../components/Navbar';
+
+interface AppLayoutProps {
+    children:ReactElement
+}
+
+const AppLayout: FunctionComponent<AppLayoutProps> = ({children}) => {
+  return (
+    <>
+      <Head>
+        <title>Countries Explorer</title>
+        <meta
+          name="description"
+          content="A next.js based frontend application that uses the REST Countries V2 API to pull and
+          display country related information"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+export default AppLayout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import Dropdown from "../components/Dropdown";
+import SearchInput from "../components/SearchInput";
 
 export default function Home() {
   return (
-    <>
-      ethem
-    </>
-  )
+    <main className="pt-16 md:pt-32 px-8 md:px-32 pb-12 md:pb-0 min-h-screen bg-dmlm-white dark:bg-dm-very-dark-blue">
+      <div className="flex justify-between flex-col md:flex-row gap-8 md:gap-1 mb-10">
+        <SearchInput />
+        <Dropdown />
+      </div>
+    </main>
+  );
 }
