f3392434236d1a7ac35c66e70d3539d2ced07114
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-22 03:00:06 +0300

    feat: dropdown ve searchınput added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import Dropdown from "../components/Dropdown";
 import SearchInput from "../components/SearchInput";
+import {useState} from "react"
 
 export default function Home() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState("");
+  const [searchValue, setSearchValue] = useState("");
   return (
     <main className="pt-16 md:pt-32 px-8 md:px-32 pb-12 md:pb-0 min-h-screen bg-dmlm-white dark:bg-dm-very-dark-blue">
       <div className="flex justify-between flex-col md:flex-row gap-8 md:gap-1 mb-10">
-        <SearchInput />
-        <Dropdown />
+        <SearchInput
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+        <Dropdown
+          isDropdownOpen={isDropdownOpen}
+          setIsDropdownOpen={setIsDropdownOpen}
+          selectedFilter={selectedFilter}
+          setSelectedFilter={setSelectedFilter}
+          filterArray={[
+            "Africa",
+            "Americas",
+            "Asia",
+            "Europe",
+            "Oceania",
+            "Show All",
+          ]}
+        />
       </div>
     </main>
   );
